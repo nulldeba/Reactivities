2f464d7856b053a9ad63a39bb1063435ed1d0c05
nulldeba <nulldebashis@gmail.com>
2021-03-03 12:47:14 +0530

    Seeding and Ef Core integration with initial setup

---
+++ Persistance/DataContext.cs
@@ -0,0 +1,16 @@
+
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistance
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        
+        public DbSet<Activity> Activities { get; set; }
+
+    }
+}
